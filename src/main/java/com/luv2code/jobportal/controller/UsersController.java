d20ce1c0c6f0b6a42f3df131170c2bc0ef7879ff
acerquone <andres.cerquone@gmail.com>
2024-08-14 10:50:55 -0300

    Show dashboard of the current logged user. Templates (Provided by the instructor): dashboard, login. Controller: Job posts.

--- src/main/java/com/luv2code/jobportal/controller/UsersController.java
+++ src/main/java/com/luv2code/jobportal/controller/UsersController.java
@@ -4,9 +4,14 @@ import com.luv2code.jobportal.entity.Users;
 import com.luv2code.jobportal.entity.UsersTypes;
 import com.luv2code.jobportal.service.UsersService;
 import com.luv2code.jobportal.service.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,5 +55,22 @@ public class UsersController {
 
         this.usersService.registerNewUser(users);
         return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+
+        return "redirect:/";
+
     }
 }
--- src/main/java/com/luv2code/jobportal/service/UsersService.java
+++ src/main/java/com/luv2code/jobportal/service/UsersService.java
@@ -7,6 +7,11 @@ import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
 import com.luv2code.jobportal.repository.RecruiterProfileRepository;
 import com.luv2code.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -49,5 +54,24 @@ public class UsersService {
     public Optional<Users> getUserByEmail(String email) {
 
         return usersRepository.getUserByEmail(email);
+    }
+
+    public Object getCurretnUserProfile() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users user = usersRepository.getUserByEmail(username).orElseThrow(()->new UsernameNotFoundException("Username not found."));
+            int userId = user.getUserId();
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                RecruiterProfile recruiterProfile = recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+                return recruiterProfile;
+            } else {
+                JobSeekerProfile jobSeekerProfile = jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+                return  jobSeekerProfile;
+            }
+        }
+
+        return null;
     }
 }
